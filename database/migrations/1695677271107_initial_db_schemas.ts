3f57053ee0dbb4e09de89d3c4b72b0d4df54bc80
Masho Ado <mashoado@gmail.com>
2023-10-07 16:10:10 +0300

    fix field types, add more fields for intial db schema migration

--- database/migrations/1695677271107_initial_db_schemas.ts
+++ database/migrations/1695677271107_initial_db_schemas.ts
@@ -6,14 +6,14 @@ export default class extends BaseSchema {
       this.schema.createTable("user", (table) => {
         table.increments("id").primary();
         table
-          .string("phone_number", 20)
+          .integer("phone_number", 20)
           .notNullable()
           .unsigned()
           .unique()
           .index();
         table.string("first_name", 255).notNullable();
         table.string("last_name", 255).notNullable();
-        table.string("password_hash");
+        table.string("password_hash").notNullable();
         table.timestamps();
       });
     }
@@ -22,7 +22,7 @@ export default class extends BaseSchema {
       this.schema.createTable("user_session", (table) => {
         table.increments("id").primary();
         table.integer("user_id").notNullable().references("id").inTable("user");
-        table.string("session_token");
+        table.string("session_token").notNullable();
         table.string("user_agent").nullable();
         table.string("ip_address").nullable();
         table.dateTime("expires_at", { useTz: true });
@@ -33,7 +33,7 @@ export default class extends BaseSchema {
     if (!(await this.schema.hasTable("product"))) {
       this.schema.createTable("product", (table) => {
         table.increments("id").primary();
-        table.string("name").unique();
+        table.string("name").unique().notNullable();
         table.timestamps();
       });
     }
@@ -41,8 +41,10 @@ export default class extends BaseSchema {
     if (!(await this.schema.hasTable("product_offer"))) {
       this.schema.createTable("product_offer", (table) => {
         table.increments("id").primary();
-        table.double("sale_price").unsigned();
-        table.integer("available_qty");
+        table.double("sale_price").unsigned().notNullable();
+        table.integer("available_qty").notNullable();
+        table.string("color").notNullable();
+        table.string("img_url").nullable();
         table.integer("product_id").references("id").inTable("product");
         table.timestamps();
       });
@@ -53,9 +55,10 @@ export default class extends BaseSchema {
         table.increments("id").primary();
         table
           .integer("product_offer_id")
-          .references("product_offer")
-          .inTable("product_offer");
-        table.integer("qty").unsigned();
+          .references("id")
+          .inTable("product_offer")
+          .notNullable();
+        table.integer("qty").unsigned().notNullable();
         table.enum("trail_direction", ["In,Out"]);
         table.timestamps();
       });
@@ -64,10 +67,15 @@ export default class extends BaseSchema {
     if (!(await this.schema.hasTable("financial_trail"))) {
       this.schema.createTable("financial_trail", (table) => {
         table.increments("id").primary();
-        table.double("amount").unsigned();
-        table.integer("usd_rate").unsigned();
-        table.enum("transaction_form", ["Cash", "Bank", "Mpesa"]);
-        table.enum("trail_direction", ["In", "Out"]);
+        table.double("amount").unsigned().notNullable();
+        table
+          .integer("usd_rate")
+          .unsigned()
+          .comment(
+            "this is the rate at which the usd was traded at, This value will not necessarily match the usd_rate from the bank"
+          );
+        table.enum("transaction_form", ["Cash", "Bank", "Mpesa"]).notNullable();
+        table.enum("trail_direction", ["In", "Out"]).notNullable();
         table.timestamps();
       });
     }
