98d7b1e1ecb9133ad7a60c791d65549a473e997f
Masho Ado <mashoado@gmail.com>
2023-10-07 16:08:52 +0300

    product success codes

--- app/Core/Constants/SuccessCodes.ts
+++ app/Core/Constants/SuccessCodes.ts
@@ -2,4 +2,11 @@ export class SuccessCodes {
   //User Auth
   public static readonly SIGN_IN = 1000;
   public static readonly SIGN_UP = 1001;
+
+  //Product
+  public static readonly LIST_PRODUCTS = 2000;
+  public static readonly VIEW_PRODUCT = 2001;
+  public static readonly CREATE_PRODUCT = 2002;
+  public static readonly UPDATE_PRODUCT = 2003;
+  public static readonly DELETE_PRODUCT = 2004;
 }
