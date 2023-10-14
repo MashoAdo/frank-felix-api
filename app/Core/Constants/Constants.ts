148472081c5e6cbe38f6e3638d7f8233774ee255
Masho Ado <mashoado@gmail.com>
2023-10-14 20:35:09 +0300

    refactor

--- app/Core/Constants/Constants.ts
+++ app/Core/Constants/Constants.ts
@@ -3,7 +3,7 @@ export default class Constants {
   public static readonly SESSION_EXPIRY_EXTEND_MINUTES = 60 * 24;
 
   public static readonly LUXON_SQL_FORMAT = "yyyy-LL-dd HH:mm:ss";
-  public static readonly LUXON_SQL_DATE_FORMAT = "yyyy-LL-dd";
+  public static readonly LUXON_DISPLAY_DATE_FORMAT = "yyyy-LL-dd";
 
   public static readonly PAGE_LIMIT = 20;
 }
